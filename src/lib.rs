a31274dc1143f2bcc0b34bfc33c487fe83a861bf
Taite Nazifi <taitenazifi@gmail.com>
2023-10-17 23:20:16 -0700

    fix: use ref

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 /*
-                                       [A+B+C+D+E+F+G+H+I+J+K+L+M+N+O]
+
+
+                                        [A+B+C+D+E+F+G+H+I+J+K+L+M+N+O]
                 __________________________________/        \________________________________
             [A+B+C+D+E+F+G+H]                                      [I+J+K+L+M+N+O]
           ______/      \_____                                  ______/      \_____
@@ -9,7 +11,7 @@
  /   \         /   \    /   \      /   \          /   \       /   \         /   \
 A     B       C     D  E     F    G     H        I     J     K     L       M     N
 
- */
+*/
 use std::collections::VecDeque;
 
 // https://en.algorithmica.org/hpc/data-structures/binary-search#eytzinger-layout
@@ -21,8 +23,8 @@ use std::collections::VecDeque;
 #[derive(Clone, Debug, Copy, PartialEq)]
 // Span is a half-open interval [start, end)
 pub struct Span {
-    pub start: u64,
-    pub end: u64,
+    pub start: usize,
+    pub end: usize,
 }
 
 impl Default for Span {
@@ -59,37 +61,123 @@ pub struct ISegmentIndex {
 }
 
 impl ISegmentIndex {
-    pub fn new(span: Span) -> Self {
-        // find the smallest power of 2 that is greater than or equal to the range of the span
-        // and then multiply it by 2 to account for the internal nodes in a complete binary tree.
-        let tree_size = 2 * (2usize.pow(((span.end - span.start) as f64).log2().ceil() as u32)) - 1;
-        Self {
+    pub fn new(values: Vec<ISegment>) -> Self {
+        let tree_size = 2 * (2usize.pow(((values.len()) as f64).log2().ceil() as u32)) - 1;
+        let mut seg_forest = Self {
             tree: vec![ISegment::default(); tree_size],
+        };
+        seg_forest.build(&values, 0, 0, values.len() - 1);
+        seg_forest
+    }
+
+    pub fn build(&mut self, values: &[ISegment], index: usize, left: usize, right: usize) {
+        if left == right {
+            if left < values.len() {
+                self.tree[index] = values[left];
+            }
+        } else {
+            let mid: usize = left + (right - left) / 2;
+            self.build(values, index * 2 + 1, left, mid);
+            self.build(values, index * 2 + 2, mid + 1, right);
+
+            let left_child = self.tree[index * 2 + 1];
+            let right_child = self.tree[index * 2 + 2];
+
+            self.tree[index] = combine(left_child, right_child);
         }
     }
 
-    pub fn build(&mut self, values: Vec<ISegment>, index: usize, left: usize, right: usize) {
-        if left == right {
-            self.tree[index] = values[left].clone();
-        } else {
-            let mid: usize = left + (right - left) / 2;
-            self.build(values.clone(), index * 2 + 1, left, mid);
-            self.build(values.clone(), index * 2 + 2, mid + 1, right);
-            self.tree[index] = ISegment {
-                span: Span {
-                    start: self.tree[index * 2 + 1].span.start,
-                    end: self.tree[index * 2 + 2].span.end,
-                },
-                count: self.tree[index * 2 + 1].count + self.tree[index * 2 + 2].count,
-                max: self.tree[index * 2 + 1]
-                    .max
-                    .max(self.tree[index * 2 + 2].max),
-                min: self.tree[index * 2 + 1]
-                    .min
-                    .min(self.tree[index * 2 + 2].min),
-                sum: self.tree[index * 2 + 1].sum + self.tree[index * 2 + 2].sum,
-            };
+    pub fn append(&mut self, value: ISegment) {
+        let tree_size = self.tree.len();
+        let mut new_value_index = (tree_size + 1) / 2;
+
+        if new_value_index * 2 >= tree_size {
+            // Double the size of the tree to accommodate the new value.
+            let new_tree_size = tree_size * 2 + 1;
+            self.tree.resize(new_tree_size, ISegment::default());
         }
+
+        // Insert the new value at the appropriate leaf position.
+        self.tree[new_value_index] = value;
+
+        // Update the internal nodes.
+        while new_value_index > 0 {
+            new_value_index = (new_value_index - 1) / 2;
+
+            let left_child_index = new_value_index * 2 + 1;
+            let right_child_index = new_value_index * 2 + 2;
+
+            self.tree[new_value_index] =
+                combine(self.tree[left_child_index], self.tree[right_child_index])
+        }
+    }
+
+    pub fn update(&mut self, target_start: usize, value: ISegment) {
+        fn update_recursive(
+            tree: &mut Vec<ISegment>,
+            node_index: usize,
+            target_start: usize,
+            value: &ISegment,
+        ) {
+            if target_start >= tree[node_index].span.start
+                && target_start <= tree[node_index].span.end
+            {
+                if tree[node_index].span.start == tree[node_index].span.end {
+                    tree[node_index] = value.clone();
+                } else {
+                    let left_child_index = node_index * 2 + 1;
+                    let right_child_index = node_index * 2 + 2;
+
+                    update_recursive(tree, left_child_index, target_start, value);
+                    update_recursive(tree, right_child_index, target_start, value);
+
+                    let left_child = &tree[left_child_index];
+                    let right_child = &tree[right_child_index];
+
+                    tree[node_index] = ISegment {
+                        span: Span {
+                            start: left_child.span.start,
+                            end: right_child.span.end,
+                        },
+                        count: left_child.count + right_child.count,
+                        max: left_child.max.max(right_child.max),
+                        min: left_child.min.min(right_child.min),
+                        sum: left_child.sum + right_child.sum,
+                    };
+                }
+            }
+        }
+
+        update_recursive(&mut self.tree, 0, target_start, &value);
+    }
+
+    pub fn print_tree(&self) {
+        fn print_node_recursive(
+            tree: &Vec<ISegment>,
+            node_index: usize,
+            depth: usize,
+            is_right: bool,
+        ) {
+            if node_index >= tree.len() {
+                return;
+            }
+
+            let left_child_index = node_index * 2 + 1;
+            let right_child_index = node_index * 2 + 2;
+
+            print_node_recursive(tree, right_child_index, depth + 1, true);
+
+            let indent = "      ".repeat(depth);
+            let branch = if is_right { " /" } else { " \\" };
+            print!("{}{}", indent, branch);
+            print!("----");
+
+            print!("<{:?},{}>", node_index, tree[node_index].sum);
+            println!();
+
+            print_node_recursive(tree, left_child_index, depth + 1, false);
+        }
+        print_node_recursive(&self.tree, 0, 0, true);
     }
 
     pub fn query_bfs(&self, query_span: Span) -> Option<ISegment> {
@@ -174,15 +262,28 @@ impl ISegmentIndex {
     }
 }
 
+fn combine(left: ISegment, right: ISegment) -> ISegment {
+    return ISegment {
+        span: Span {
+            start: left.span.start,
+            end: right.span.end,
+        },
+        count: left.count + right.count,
+        max: left.max.max(right.max),
+        min: left.min.min(right.min),
+        sum: left.sum + right.sum,
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::{ISegment, ISegmentIndex, Span};
 
     fn tree_data() -> (Vec<ISegment>, ISegmentIndex) {
-        let mut data = vec![ISegment::default(); 6];
+        let mut data: Vec<ISegment> = vec![ISegment::default(); 6];
         for i in 0..data.len() {
-            let time: u64 = i as u64;
-            let val: f64 = i as f64 * 2.0;
+            let time: usize = i;
+            let val: f64 = i as f64;
             data[i] = ISegment {
                 count: 1,
                 max: val,
@@ -195,19 +296,13 @@ mod tests {
             };
         }
 
-        (
-            data.clone(),
-            ISegmentIndex::new(Span {
-                start: data[0].span.start,
-                end: data[data.len() - 1].span.end,
-            }),
-        )
+        (data.clone(), ISegmentIndex::new(data))
     }
 
     #[test]
     fn build() {
         let (data, mut tree) = tree_data();
-        tree.build(data.clone(), 0, 0, data.len() - 1);
+        tree.build(&data, 0, 0, data.len() - 1);
         for i in 0..tree.tree.len() {
             println!("{:?}", tree.tree[i]);
         }
@@ -218,79 +313,77 @@ mod tests {
     #[test]
     fn sum() {
         let (data, mut tree) = tree_data();
-        tree.build(data.clone(), 0, 0, data.len() - 1);
+        tree.build(&data, 0, 0, data.len() - 1);
 
         for i in 0..data.len() {
             print!("{:?} ", data[i].sum);
         }
+
         for i in 0..tree.tree.len() {
             println!("{:?}", tree.tree[i]);
         }
 
         assert_eq!(
             tree.query_bfs(Span { start: 1, end: 6 },).unwrap().sum,
-            30.0,
+            15.0,
         );
 
         assert_eq!(
             tree.query_bfs(Span { start: 1, end: 6 },).unwrap().sum,
-            30.0,
+            15.0,
         );
 
-        assert_eq!(tree.query_bfs(Span { start: 1, end: 3 },).unwrap().sum, 6.0);
+        assert_eq!(tree.query_bfs(Span { start: 1, end: 3 },).unwrap().sum, 3.0);
 
         assert_eq!(
             tree.query_bfs(Span { start: 0, end: 6 },).unwrap().sum,
-            30.0
+            15.0
         );
     }
 
     #[test]
     fn max() {
         let (data, mut tree) = tree_data();
-        tree.build(data.clone(), 0, 0, data.len() - 1);
+        tree.build(&data, 0, 0, data.len() - 1);
 
-        assert_eq!(
-            tree.query_bfs(Span { start: 2, end: 6 },).unwrap().max,
-            10.0,
-        );
+        assert_eq!(tree.query_bfs(Span { start: 2, end: 6 },).unwrap().max, 5.0,);
 
         assert_eq!(
             tree.query_dfs(0, Span { start: 1, end: 3 },).unwrap().max,
-            4.0
+            2.0
         );
 
         assert_eq!(
             tree.query_dfs(0, Span { start: 1, end: 7 },).unwrap().max,
-            10.0
+            5.0
         );
     }
 
     #[test]
     fn min() {
         let (data, mut tree) = tree_data();
-        tree.build(data.clone(), 0, 0, data.len() - 1);
+        tree.build(&data, 0, 0, data.len() - 1);
 
         assert_eq!(
             tree.query_dfs(0, Span { start: 2, end: 6 },).unwrap().min,
-            4.0,
+            2.0,
         );
 
         assert_eq!(
             tree.query_dfs(0, Span { start: 1, end: 3 },).unwrap().min,
-            2.0
+            1.0
         );
 
         assert_eq!(
             tree.query_dfs(0, Span { start: 1, end: 7 },).unwrap().min,
-            2.0
+            1.0
         );
     }
 
     #[test]
     fn count() {
         let (data, mut tree) = tree_data();
-        tree.build(data.clone(), 0, 0, data.len() - 1);
+        tree.build(&data, 0, 0, data.len() - 1);
 
         assert_eq!(
             tree.query_dfs(0, Span { start: 2, end: 6 },).unwrap().count,
@@ -306,6 +399,7 @@ mod tests {
             tree.query_dfs(0, Span { start: 1, end: 6 },).unwrap().count,
             5
         );
+
         assert_eq!(
             tree.query_dfs(0, Span { start: 0, end: 6 },).unwrap().count,
             6
